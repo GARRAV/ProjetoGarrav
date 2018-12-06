424b6b7c395080d9da133be506f921f3a43d6023
CynDrai <cyndrai@users.noreply.github.com>
2018-12-06 18:50:36 -0200

    Implementação da API Google Maps
    -Versão do App: 0.0.1 -> 0.1.2;
    
    -API do Google Maps adicionada em Fragment e implementada em MapsEventsActivity.java em um container de dados;
    
    -Criação da classe MapsFragment.java que instânciará o mapa do Google Maps da Activity;
    
    -Container de dados - FrameLayout - criado em content_maps_events.xml;
    
    -Criação de google_maps_api.xml, que gerência a chave da API do GoogleMaps.

--- app/src/main/java/br/com/garrav/projetogarrav/MapsEventsActivity.java
+++ app/src/main/java/br/com/garrav/projetogarrav/MapsEventsActivity.java
@@ -2,6 +2,8 @@ package br.com.garrav.projetogarrav;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,30 +15,38 @@ import android.view.MenuItem;
 public class MapsEventsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_events);
 
         //Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         //Chamado barra lateral
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         //Referência aos itens da barra lateral
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Instância do Fragment Google Maps
+        this.fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = this.fragmentManager.beginTransaction();
+        transaction.add(R.id.flMapsContainer, new MapsFragment(), "MapsFragment");
+        transaction.commitAllowingStateLoss();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -89,7 +99,7 @@ public class MapsEventsActivity extends AppCompatActivity
         }
 
         //Fecha a barra lateral após item selecionado
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
