46eb000265d1e7861886cc46afc8d1c9a078a57e
CynDrai <cyndrai@users.noreply.github.com>
2019-01-12 17:04:41 -0200

    Implemetação de ProgressDialog Síncrono e classe modelo Challenge
    -Versão do App: 0.3.0 > 0.3.1;
    
    -Adicionado classe modelo Challenge.java;
    
    -Implementado função em ChallengeActivity.java que habilita um Dialog que representa o Loading de conteúdos.

--- app/src/main/java/br/com/garrav/projetogarrav/ChallengeActivity.java
+++ app/src/main/java/br/com/garrav/projetogarrav/ChallengeActivity.java
@@ -1,10 +1,15 @@
 package br.com.garrav.projetogarrav;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.ListView;
 
 public class ChallengeActivity extends AppCompatActivity {
+
+    private ListView lvChallengeList;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,20 @@ public class ChallengeActivity extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setTitle("Desafios");
+
+        loadChallenges();
     }
+
+    /**
+     *
+     * @author Felipe Savaris
+     * @since 12/01/2019
+     */
+    public void loadChallenges() {
+        this.progressDialog = new ProgressDialog(this);
+        this.progressDialog.setMessage("Carregando...");
+        this.progressDialog.setCancelable(false);
+        this.progressDialog.show();
+    }
+
 }
