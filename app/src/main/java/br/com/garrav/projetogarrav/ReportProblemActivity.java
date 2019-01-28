1977bd4049c8c2db4376f3b676ce2b0886414d08
CynDrai <cyndrai@users.noreply.github.com>
2019-01-28 15:13:38 -0200

    Implementação de elementos da Tela Report de Bugs
    -Versão do App: 0.4.7 -> 0.4.8;
    
    -Criação de elementos do XML activity_report_problem.xml -> Um TextView, um EditText e um Button;
    
    -Criação do Método para interagir com o Button do XML;
    
    -Textos adicionados em strings.xml.

--- app/src/main/java/br/com/garrav/projetogarrav/ReportProblemActivity.java
+++ app/src/main/java/br/com/garrav/projetogarrav/ReportProblemActivity.java
@@ -1,7 +1,9 @@
 package br.com.garrav.projetogarrav;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ReportProblemActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class ReportProblemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_problem);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Reportar Problemas");
+    }
+
+    /**
+     *
+     * @param view
+     * @author Felipe Savaris
+     * @since 28/01/2019
+     */
+    public void btSendProblem(View view) {
+
     }
 }
